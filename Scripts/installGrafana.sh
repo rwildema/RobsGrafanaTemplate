a8f5d881bc8d42aac8f4435a6f379654a967b53e
Rob Wildeman <rbwildeman@outlook.com>
2020-08-26 11:49:51 -0400

    Added -y flag to install grafana

--- Scripts/installGrafana.sh
+++ Scripts/installGrafana.sh
@@ -5,11 +5,11 @@ apt-get install -y apt-transport-https
 apt-get install -y software-properties-common wget
 
 #install the latest Grafana OSS release
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add
 
 #add repo for stable releases
 echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list 
 
 #update and install
 apt-get update
-apt-get install grafana
+apt-get install grafana -y
